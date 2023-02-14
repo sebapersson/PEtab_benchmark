7ba923e6585c5c2318cb8d8f3abeec61ae1c797c
Stephan Grein <stephan.grein@uni-bonn.de>
2023-02-14 16:43:02 +0100

    Get rid of conda.

--- setup.sh
+++ setup.sh
@@ -1,26 +1,45 @@
+pathJulia=""
+# needs to be adjusted
+customPathJulia=/home/stephan/Code/julia/julia
+versionJulia=""
+requiredJuliaVersion="1.8.5"
 
-# To stay consistent let us use Julia 1.8.5
-pathJulia="/home/sebpe/julia-1.8.5-linux-x86_64/julia-1.8.5/bin/julia"
-if [ ! "$(eval "${pathJulia} --version")"  == "julia version 1.8.5" ];then
-    >&2 echo "Error : Julia path most point to Julia 1.8.5"
-    exit 1
+# first try system julia
+if [ ! -z "$(whereis julia | cut -d ':' -f 2)" ]; then
+   echo "Using system's julia if available"
+   pathJulia=$(whereis julia | cut -d ':' -f 1)
+   versionJulia=$(whereis julia | cut -d ':' -f 2)
 fi
 
-# To use Conda (Fides in Julia)
-eval "$(conda shell.bash hook)"
+# if version not the required, use custom julia installation
+if [ "$versionJulia" != "$requiredJuliaVersion" ]; then
+   echo -n "System's Julia outdated, trying custom julia installation..."
+   if [ ! -z "$customPathJulia" ]; then
+      pathJulia=$customPathJulia
+      versionJulia=$($pathJulia --version | awk -F ' ' '{print $3}')
+   else
+      echo "No custom Julia provided. Exiting."
+   fi
+else
+   echo "System's Julia... "
+fi
+
+if [ "$versionJulia" != "$requiredJuliaVersion" ]; then
+   echo "Neither system nor custom julia installation version satisfied. Exiting."
+   exit 1
+else
+   echo " found!"
+fi
 
 # Set up Julia PEtab package
 git clone https://github.com/CleonII/Master-Thesis.git
 cd Master-Thesis
 echo "Installing needed Python packages"
-conda env create -f PeTab.yml
-echo "Done"
-conda activate PeTab
 echo "Installing Julia packages (might take some time https://xkcd.com/303/)"
 ${pathJulia} --project=. -e"using Pkg; Pkg.instantiate()"
 echo "Done"
 
-# Set up PyPesto and AMICI
+ Set up PyPesto and AMICI
 cd ../pypesto_benchmark
 bash setup.sh
 
