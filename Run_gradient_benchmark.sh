2e964d0ee3065e64ec1a4b3bd1a5c68e6035c379
sebapersson <sebastian.persson1@gmail.com>
2023-08-14 19:33:19 +0200

    Update gradient benchmark

--- Run_gradient_benchmark.sh
+++ Run_gradient_benchmark.sh
@@ -61,10 +61,20 @@ if [ $1 == "Test_adjoint_random_julia" ];then
 fi 
 
 
-if [ $1 == "Test_adjoint_random_amici" ];then
+if [ $1 == "Test_adjoint_random" ];then
+
+    # Julia
+    cd Julia_adjoint
+    runJulia="/home/sebpe/julia-1.8.5-linux-x86_64/julia-1.8.5/bin/julia --project=. --threads=1"
+    ${runJulia} Test_adjoint_random_p Boehm_JProteomeRes2014
+    ${runJulia} Test_adjoint_random_p Bachmann_MSB2011
+    ${runJulia} Test_adjoint_random_p Lucarelli_CellSystems2018
+    ${runJulia} Test_adjoint_random_p Smith_BMCSystBiol2013
+    cd ..
+
+    # Python (AMICI)
     python amici_gradient_benchmark/gradient_benchmark_amici.py Boehm_JProteomeRes2014
     python amici_gradient_benchmark/gradient_benchmark_amici.py Bachmann_MSB2011
     python amici_gradient_benchmark/gradient_benchmark_amici.py Lucarelli_CellSystems2018
-    python amici_gradient_benchmark/gradient_benchmark_amici.py Isensee_JCB2018 
     python amici_gradient_benchmark/gradient_benchmark_amici.py Smith_BMCSystBiol2013 
 fi 
